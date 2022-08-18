9232c583310d8a621968b80281d83917b0249f4b
HamDenRoede <cph-ji63@cphbusiness.dk>
2022-08-18 09:51:08 +0200

    Everything works :D

--- Program.cs
+++ Program.cs
@@ -7,32 +7,35 @@ namespace RandomExercises
         static void Main(string[] args)
         {
             Terning();
+            sixCount();
         }
 
         static void Terning()
         {
             Random value = new Random();
-
-           
             
+            for (int i = 1; i <= 10; i++)
+            {
+                int terning = value.Next(1, 7);
+                Console.WriteLine(terning);
+            }
+            Console.WriteLine("Done with 10 dice \n");
+        }
 
+        static void sixCount()
+        {
+            Random value = new Random();
             int sixDice = 0;
-            string indicator = " ";
             do
             {
                 int terning = value.Next(1, 7);
                 Console.WriteLine(terning);
-                if ("6".Contains(indicator))
+                if (terning == 6)
                 {
                     sixDice++;
                 }
-                for (int i = 1; i <= 10; i++)
-                {
-
-                    //int terning = value.Next(1, 7);
-                    Console.WriteLine(terning);
-                }
-            } while (sixDice == 6);
+            } while (sixDice <= 5);
+            Console.WriteLine("Tillykke, du har slået 6x seksere");
         }
     }
 }
