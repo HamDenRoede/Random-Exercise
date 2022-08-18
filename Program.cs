65482e97373e481ff2a97ee218d236cb694699ad
HamDenRoede <cph-ji63@cphbusiness.dk>
2022-08-18 10:00:27 +0200

    NU MED COMMENTS! :D:D:D:D:D

--- Program.cs
+++ Program.cs
@@ -6,23 +6,29 @@ namespace RandomExercises
     {
         static void Main(string[] args)
         {
-
+            //her er vores to metode-kald
             Terning();
             sixCount();
         }
 
         static void Terning()
         {
-
+            
             Console.WriteLine("Throwing 10 dice");
+
+            //her sætter vi en tilfældig variable via "Random" metoden
             Random value = new Random();
+
+            //her starter vi et for loop, som looper 10 gange (se "i <=10)
             for (int i = 1; i <= 10; i++)
             {
 
+                //her kastes vores terning. terningen er sekssidet. Det kan ses i "value.Next(1, 7);"
                 int terning = value.Next(1, 7);
                 Console.WriteLine(terning);
             }
 
+            //loop færdig
             Console.WriteLine("Done with 10 dice \n");
         }
 
@@ -30,17 +36,24 @@ namespace RandomExercises
         {
 
             Console.WriteLine("Throwing dice until 6x sixes");
+
+            //her sætter vi igen variabler. Denne gang sætter vi også en variable som skal tælles hver gang terningen lander på "6"
             Random value = new Random();
             int sixDice = 0;
             do
             {
+
+                //i dette loop kaster vi 1 terning ad gangen, i modsætning til 10 på én som i det tidligere loop
                 int terning = value.Next(1, 7);
                 Console.WriteLine(terning);
+
+                //Her beder vi om at vores sixDice variable tæller op (incrementer), hver gang terningen lander på 6
                 if (terning == 6)
                 {
                     sixDice++;
                 }
             } while (sixDice <= 5);
+            //loopet stoppes ved 6x seksere
             Console.WriteLine("Tillykke, du har slået 6x seksere");
         }
     }
