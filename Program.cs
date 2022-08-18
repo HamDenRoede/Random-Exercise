b1a43893e6e7bc04045c01b0837ded87f9d7dbe6
HamDenRoede <cph-ji63@cphbusiness.dk>
2022-08-18 08:53:01 +0200

    dice for-loop

--- Program.cs
+++ Program.cs
@@ -4,9 +4,24 @@ namespace RandomExercises
 {
     class Program
     {
+        static Random value = new Random();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Terning();
+        }
+
+        static void Terning()
+        {
+            
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Random value = new Random();
+                int terning = value.Next(1, 7);
+                Console.WriteLine(terning);
+
+            }
         }
     }
 }
