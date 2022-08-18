1589b995c92259a948789e27b49e88681ac50c47
HamDenRoede <cph-ji63@cphbusiness.dk>
2022-08-18 09:52:38 +0200

    everything works..... NOW (I hope)

--- Program.cs
+++ Program.cs
@@ -6,24 +6,30 @@ namespace RandomExercises
     {
         static void Main(string[] args)
         {
+
             Terning();
             sixCount();
         }
 
         static void Terning()
         {
+
+            Console.WriteLine("Throwing 10 dice");
             Random value = new Random();
-            
             for (int i = 1; i <= 10; i++)
             {
+
                 int terning = value.Next(1, 7);
                 Console.WriteLine(terning);
             }
+
             Console.WriteLine("Done with 10 dice \n");
         }
 
         static void sixCount()
         {
+
+            Console.WriteLine("Throwing dice until 6x sixes");
             Random value = new Random();
             int sixDice = 0;
             do
