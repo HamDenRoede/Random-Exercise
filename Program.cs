ad330b534a7e636691cabd4a64e989e80e6286ed
HamDenRoede <cph-ji63@cphbusiness.dk>
2022-08-18 11:48:41 +0200

    added ned switch method

--- Program.cs
+++ Program.cs
@@ -7,8 +7,9 @@ namespace RandomExercises
         static void Main(string[] args)
         {
             //her er vores to metode-kald
-            Terning();
-            sixCount();
+            //Terning();
+            //sixCount();
+            switchCount();
         }
 
         static void Terning()
@@ -55,6 +56,59 @@ namespace RandomExercises
             } while (sixDice <= 5);
             //loopet stoppes ved 6x seksere
             Console.WriteLine("Tillykke, du har slået 6x seksere");
+        }
+
+        static void switchCount()
+        {
+            
+            for (int i = 1; i <= 10; i++)
+            {
+
+                Random value = new Random();
+                int rand = value.Next(1, 7);
+
+                int antalEttere = 0;
+                int antalToere = 0;
+                int antalTreere = 0;
+                int antalFirere = 0;
+                int antalFemmere = 0;
+                int antalSeksere = 0;
+
+                //her kastes vores terning. terningen er sekssidet. Det kan ses i "value.Next(1, 7);"
+                //int terning = value.Next(1, 7);
+                //int startValue = 1;
+                //int input = int.Parse(Console.ReadLine());
+                //Console.WriteLine(value);
+
+                switch (rand)
+                {
+                    case 1:
+                            antalEttere++;
+                        Console.WriteLine("Du slog en etter");
+                            break;
+                    case 2:
+                            antalToere++;
+                        Console.WriteLine("Du slog en toer");
+                        break;
+                    case 3:
+                            antalTreere++;
+                        Console.WriteLine("Du slog en treer");
+                        break;
+                    case 4:
+                            antalFirere++;
+                        Console.WriteLine("Du slog en firer");
+                        break;
+                    case 5:
+                            antalFemmere++;
+                        Console.WriteLine("Du slog en femmer");
+                        break;
+                    case 6:
+                            antalSeksere++;
+                        Console.WriteLine("Du slog en sekser");
+                        break;
+                        
+                }
+            }
         }
     }
 }
